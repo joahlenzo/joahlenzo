50447366cba00f14068d80e8dda4b78967251e84
joahlenzo <joah.lenzo@me.com>
2022-06-03 16:14:29 +0200

    Ganzes Python wiederhergestellt!!!!!!!!!!!!

---
+++ Datenbankabfrage.py
@@ -0,0 +1,32 @@
+import json
+
+
+#json-file wird geschrieben mit w = write
+def speichern(input_ausgaben, input_kategorie, input_datum):
+    datei = "datenbank.json"
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = []
+        print("Sorry, finde nix mit diese")
+
+    inhalt = {"Ausgaben": input_ausgaben,
+              "Kategorie": input_kategorie,
+              "Datum": input_datum}
+    datei_inhalt.append(inhalt)
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file, indent=3)
+
+#funktion um json file zu laden
+def eingabe_laden():
+    datei_name = "datenbank.json"
+
+    try:
+        with open(datei_name) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = []
+
+    return datei_inhalt
---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import Datenbankabfrage
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # get = Inhalt von eingabe.html dem Client anzeigen
+    if request.method.lower() == "get":
+        return render_template('index.html')
+    # post = ausgefüllte Daten vom Formular von Client via url /eingabe erhalten
+    if request.method.lower() == "post":
+        input_ausgaben = int(request.form['Ausgaben'])
+        input_kategorie = request.form["Kategorie"]
+        input_datum = request.form["Datum"]
+
+        Datenbankabfrage.speichern(input_ausgaben,input_kategorie,input_datum) #Speicherung Daten
+
+
+        eingabe_gespeichert = "Dein Eintrag wurde erfolgreich gespeichert" #Nach Sendebutton anzeigen
+        return render_template("index.html", eingabe=eingabe_gespeichert)
+    return render_template("index.html")
+
+
+@app.route("/ausgaben",methods=["GET", "POST"])
+def ausgaben():
+    # get = Inhalt der Finanzen.html wird angezeigt
+    if request.method.lower() == "get":
+        return render_template("Ausgaben.html")
+    # post = Formulardaten erhalten
+    if request.method.lower() == "post":
+        kategorie_filter = request.form['Kategorie']
+        monat_filterung = request.form['Monat']
+
+        # Daten filtern und zusammenzählen
+        daten_filtern = Datenbankabfrage.eingabe_laden()
+        gefilterte_elemente = []
+        summe = 0
+        for liste_elemente in daten_filtern:
+            if liste_elemente['Kategorie'] == kategorie_filter and liste_elemente['Datum'].split('-')[1] == monat_filterung:
+                gefilterte_elemente.append(liste_elemente)
+                summe += liste_elemente["Ausgaben"]
+            Aussagesatz = "Du gibst im " + monat_filterung + "-ten Monat " + str(summe) + " SFR nur für die Kategorie " + kategorie_filter + " aus!"
+        return render_template("Ausgaben.html", liste_elemente=gefilterte_elemente, summe=summe, aussagesatz=Aussagesatz)
+    return render_template("Ausgaben.html")
+
+
+@app.route("/finanzen")
+def finanzen():
+    daten_filtern = Datenbankabfrage.eingabe_laden()
+    kategorien = []
+    summenliste = []
+    for elemente in daten_filtern:
+        kategorien_elemente = elemente["Kategorie"]
+        kategorien.append(kategorien_elemente)
+        summen = elemente["Ausgaben"]
+        summenliste.append(summen)
+    return render_template("Finanzen.html", kategorien=kategorien, summenliste=summenliste)
+
+
+@app.route("/jahresziel")
+def jahresziel():
+    Jahresziel = 30000
+    summe_ausgaben = 0
+    daten_zählen = Datenbankabfrage.eingabe_laden()
+    for elemente in daten_zählen:
+        summe_ausgaben += elemente["Ausgaben"]
+        goal_jahresziel = Jahresziel - summe_ausgaben
+    return render_template("Jahresziel.html", summiert=summe_ausgaben, jahresziel=goal_jahresziel, jahresziel_ungerechnet=Jahresziel)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
